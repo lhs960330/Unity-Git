d84e15fd862312ce68991987ff2bf0e0f10cfd42
효석 이 <lhs19960330@gmail.com>
2024-01-30 21:34:00 +0900

    feat : 카메라 고정

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offSet;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offSet = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + offSet;
     }
 }
