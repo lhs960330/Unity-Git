e272f1bb60fd9e28e55bc56e7557d808a06c55c7
Lee Hyoseok <lhs19960330@gmail.com>
2024-02-02 09:46:47 +0900

    feat : 탱크 머리움직이기

---
+++ Assets/Spin.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Spin : MonoBehaviour
+{
+
+    public float spinSpeed;
+    Vector3 vector3;
+
+
+    private void OnSpin(InputValue value)
+    {
+        Vector2 imputDir = value.Get<Vector2>();
+        vector3.x = imputDir.x;
+        vector3.y = imputDir.y;
+    }
+    private void Rotate()
+    {
+
+        transform.Rotate(Vector3.up, vector3.x * spinSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.right, -vector3.y * spinSpeed * Time.deltaTime, Space.Self);
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+            Rotate();
+       
+    }
+}
